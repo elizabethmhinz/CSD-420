47b5d72ed2938dedd18a4183dede84a3a6139efd
elizabethmhinz <emhinz@my365.bellevue.edu>
2025-05-04 12:34:20 -0700

    pushing modules

---
+++ module-9/codeExamples/Select5.java
@@ -0,0 +1,53 @@
+/*
+ *
+ * Professor Darrell Payne
+ * Bellevue University
+ *
+ */
+import java.sql.*;
+
+public class Select5{
+
+  public static void main(String args[]){
+
+    try{
+
+      Connection con;
+
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      // String url = "jdbc:mysql://localhost:3306/database33?";
+      String url = "jdbc:mysql://localhost:3306/database33?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+      //con = DriverManager.getConnection(url + "user=student33&password=Passw0rd");
+      con = DriverManager.getConnection(url, "student33", "Passw0rd");
+
+
+      System.out.println("Connection established - now executing a select");
+
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM address33");
+
+     System.out.println("Received Results:");
+
+     int i = rs.getMetaData().getColumnCount();
+
+     while(rs.next()){
+
+       for(int x = 1; x <= i; ++x){
+
+         System.out.println(rs.getString(x));
+       }
+
+       System.out.println("");
+     }
+
+     stmt.close();
+     con.close();
+    }
+    catch (java.lang.Exception ex){
+
+      ex.printStackTrace();
+    }
+  }
+}
